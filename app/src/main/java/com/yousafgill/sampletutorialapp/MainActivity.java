02f77d388a0d8aa08b05bd1df379a0e63f62f096
yousafgill <yousafgill@gmail.com>
2017-03-17 23:23:51 +0500

    Added button variable in MainActivity

--- app/src/main/java/com/yousafgill/sampletutorialapp/MainActivity.java
+++ app/src/main/java/com/yousafgill/sampletutorialapp/MainActivity.java
@@ -2,9 +2,10 @@ package com.yousafgill.sampletutorialapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btnsave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
